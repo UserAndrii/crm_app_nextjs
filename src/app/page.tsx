f2d0f5e8d546be3d5f50bacb4d89420d34b28942
Andrii Gadar <gadar.andre@gmail.com>
2024-04-17 22:31:35 +0300

    Added company form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AddCompanyButton from './components/AddCompanyButton';
 import StatusLabel, { Status } from './components/StatusLabel';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
       <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <AddCompanyButton />
     </main>
   );
 }
