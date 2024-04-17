37f679b33977d0f0bb7ab90b2b5367b42b0a5870
Andrii Gadar <gadar.andre@gmail.com>
2024-04-17 23:30:29 +0300

    Added companies page

---
+++ src/app/companies/page.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Header from '@/app/components/Header';
+import Toolbar from '@/app/components/Toolbar';
+import SearchInput from '@/app/components/SearchInput';
+import AddCompanyButton from '@/app/components/AddCompanyButton';
+import CompanyTable from '@/app/components/CompanyTable';
+import CompanyRow from '@/app/components/CompanyRow';
+import { Status } from '@/app/components/StatusLabel';
+import companyData from './companyData';
+
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+  return (
+    <>
+      <Header>Companies</Header>
+      <main>
+        <Toolbar action={<AddCompanyButton />}>
+          <SearchInput />
+        </Toolbar>
+        <CompanyTable>
+          {companyData.map(
+            ({
+              id,
+              category,
+              company,
+              status,
+              promotion,
+              country,
+              joinedDate,
+            }) => (
+              <CompanyRow
+                key={id}
+                id={id}
+                category={category}
+                company={company}
+                status={status}
+                promotion={promotion}
+                country={country}
+                joinedDate={joinedDate}
+              />
+            ),
+          )}
+        </CompanyTable>
+      </main>
+    </>
+  );
+}
