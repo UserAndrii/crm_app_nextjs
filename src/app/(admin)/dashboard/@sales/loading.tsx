1f7e80113e3756302000e36b455a0dee767a4e62
Andrii Gadar <gadar.andre@gmail.com>
2024-04-18 15:39:53 +0300

    Added dashboard slots

---
+++ src/app/(admin)/dashboard/@sales/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface loadingProps {}
+
+const loading: React.FC<loadingProps> = () => {
+  return <p>Loading ...</p>;
+};
+
+export default loading;
--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,15 +1,11 @@
-import MagicButton from '@/app/components/MagicButton';
+import Header from '@/app/components/Header';
+// import MagicButton from '@/app/components/MagicButton';
 import React from 'react';
 
 export interface pageProps {}
 
 const page: React.FC<pageProps> = () => {
-  return (
-    <main>
-      <h1 className="text-xl">Dashboard Page</h1>
-      <MagicButton />
-    </main>
-  );
+  return <Header>Dashboard</Header>;
 };
 
 export default page;
